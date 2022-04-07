ed5c03b31dd8a556ddd522609d65b65d8e0dde2f
“liyu” <17806096859@163.com>
2022-04-07 13:41:30 +0800

    集合

--- 集合.py
+++ 集合.py
@@ -1,43 +1,42 @@
-#{}为空定义出来的是字典，{}有内容定义出来的是集合
-# a ={}#a为字典
-# b = {1}#为集合
-# c =set()#c为集合
-# print(type(a))
-# print(type(b))
-# print(type(c))
-# '''
-# 集合的长度
-# '''
-# print(len(b))
-# print(len(c))
 '''
-集合的并集
+创建集合
 '''
-# a = {1,2,3}
-# b = {1,4,5}
-# print(a.union(b))
+# * 通过使用{}填充元素
+set1 = {1, 2, 3}
+print(type(set1), set1)
+# * 通过构造方法 set()
+# 字符串
+set2 = set("hogwarts")
+print(type(set2), set2)
+# 列表
+set3 = set([1, 2, 3])
+print(type(set3), set3)
+
+# 创建空集合
+set4 = set()
+print(type(set4), set4)
+# * 通过集合推导式
+set5 = {i for i in range(1,11) if i % 2 == 0}
+print(type(set5), set5)
+
 '''
-集合的交集
+成员检测
 '''
-# a = {1,2,3}
-# b = {1,4,5}
-# print(a.intersection(b))
+set6 = {1, 2, 3, 4}
+print(1 in set6)
+print(100 not in set6)
+
 '''
-集合的差集，我有你没有
+集合方法
 '''
-# a = {1,2,3}
-# b = {1,4,5}
-# print(a.difference(b))
-'''
-集合添加元素
-'''
-# a = {1,2,3}
-# a.add("a")
-# print(a)
-'''
-集合推导式,求出去重的集合
-'''
-# print({i for i in "24234235425"})
-'''
-通过集合去重
-'''
+
+# add
+set7 = set()
+# set7.add(1)
+# set7.add(2)
+# set7.add('asdas')
+set7.update('hogwarts')
+print(set7)
+# 传入列表
+set7.update([1, 2, 3])
+print(set7)
